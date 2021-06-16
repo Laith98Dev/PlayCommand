3282e5d6e5574f322bfacf51171ebd5237470ebf
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2021-06-16 04:20:28 -0700

    Create Main.php

---
+++ src/Laith98Dev/PlayCommand/Main.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Laith98Dev\PlayCommand;
+
+/*  
+ *  A plugin for PocketMine-MP.
+ *  
+ *	 _           _ _   _    ___   ___  _____             
+ *	| |         (_) | | |  / _ \ / _ \|  __ \            
+ *	| |     __ _ _| |_| |_| (_) | (_) | |  | | _____   __
+ *	| |    / _` | | __| '_ \__, |> _ <| |  | |/ _ \ \ / /
+ *	| |___| (_| | | |_| | | |/ /| (_) | |__| |  __/\ V / 
+ *	|______\__,_|_|\__|_| |_/_/  \___/|_____/ \___| \_/  
+ *	
+ *  Copyright (C) 2021 Laith98Dev
+ *  
+ *	Youtube: Laith Youtuber
+ *	Discord: Laith98Dev#0695
+ *	Gihhub: Laith98Dev
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ */
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+	
+	public function onEnable(){
+		@mkdir($this->getDataFolder());
+		
+		$map = $this->getServer()->getCommandMap();
+		$map->register("play", new PlayCommand($this));
+		
+		if(!is_file($this->getDataFolder() . "config.yml")){
+			(new Config($this->getDataFolder() . "config.yml", Config::YAML, ["games_list" => []]));
+		}
+	}
+	
+	public function getGamesList(){
+		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		return $cfg->get("games_list", []);
+	}
+	
+	public function addGame(string $name, string $command): bool{
+		if(!is_file($this->getDataFolder() . "config.yml"))
+			return false;
+		
+		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		
+		$command = str_replace("/", "", $command);
+		$index = [];
+		
+		$list = $cfg->get("games_list", []);
+		
+		foreach ($list as $game => $cmd){
+			$index[$game] = $cmd;
+		}
+		
+		if(isset($index[$name]))
+			return false;
+		
+		$index[$name] = $command;
+		
+		$cfg->set("games_list", $index);
+		$cfg->save();
+		return true;
+	}
+	
+	public function removeGame(string $name): bool{
+		if(!is_file($this->getDataFolder() . "config.yml"))
+			return false;
+		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		$list = $cfg->get("games_list", []);
+		
+		$index = [];
+		foreach ($list as $game => $cmd){
+			$index[$game] = $cmd;
+		}
+		
+		if(isset($index[$name])){
+			unset($index[$name]);
+			$cfg->set("games_list", $index);
+			$cfg->save();
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public function editGame(string $name, string $newCommand): bool{
+		if(!is_file($this->getDataFolder() . "config.yml"))
+			return false;
+		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		
+		$list = $cfg->get("games_list", []);
+		
+		$index = [];
+		foreach ($list as $game => $cmd){
+			$index[$game] = $cmd;
+		}
+		
+		if(isset($index[$name])){
+			$newCommand = str_replace("/", "", $newCommand);
+			$index[$name] = $newCommand;
+			$cfg->set("games_list", $index);
+			$cfg->save();
+			return true;
+		}
+		
+		return false;
+	}
+}
